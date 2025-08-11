0073d16c7334766fa0cc1eefe1ab436fd3a564b3
Holden Harris <holden.earl.harris@gmail.com>
2025-08-11 16:30:03 -0400

    Streamline code with GPT to be read easier

--- Query-environmental-preferences.R
+++ Query-environmental-preferences.R
@@ -3,28 +3,33 @@
 ## Org:     Isla Mar, Harris Analytics and Research LLC
 ## Project: Caribbean CVA
 ## Contact: Holden Earl Harris | holden.earl.harris@gmail.com
-## Code:    Query Fishbase Database
-##          Find environmental preference
+## Code:    Query FishBase database for environmental and trait data
+##------------------------------------------------------------------------------
 
-rm(list=ls()); gc()
-library(dplyr)
+# Clear environment and load packages
+rm(list = ls()); gc()
 library(dplyr)
 library(rfishbase)
 
+#------------------------------------------------------------------------------
 # Load species list
-sp_list <- read.csv("./species-list.csv")  
+sp_list <- read.csv("./species-list.csv")  # Assumes a column 'scientific_name'
 
-# Query environmental and trait data from fishbase database (takes a minute)
-species_fb <- rfishbase::species(sp_list$scientific_name)
-growth_fb  <- popgrowth(sp_list$scientific_name)
-ecology_fb <- ecology(sp_list$scientific_name)
-repro_fb  <- reproduction(sp_list$scientific_name)
+#------------------------------------------------------------------------------
+# Query FishBase tables
+# These API calls may take ~1 minute depending on list size
+#------------------------------------------------------------------------------
 
+spp_raw     <- species(sp_list$scientific_name)
+growth_raw  <- popgrowth(sp_list$scientific_name)
+ecology_raw <- ecology(sp_list$scientific_name)
+repro_raw   <- reproduction(sp_list$scientific_name)
 
-# Just pull selected columns to select from species_db
+#------------------------------------------------------------------------------
+# Select and join species-level attributes
+
 selected_cols_species <- c(
-  "Species", 
-  "FBname", "BodyShapeI",
+  "Species", "FBname", "BodyShapeI",
   "DepthRangeShallow", "DepthRangeDeep", 
   "DepthRangeComShallow", "DepthRangeComDeep", 
   "LongevityWild", "LongevityCaptive", 
@@ -32,60 +37,49 @@ selected_cols_species <- c(
   "Length", "CommonLength", "Weight"
 )
 
-# Left join the selected columns from species_db to species_list
-sp_attritbutes <- 
-  sp_list %>%
-  left_join(
-    select(species_fb, all_of(selected_cols_species)),
-    by = c("scientific_name" = "Species")
-  )
+sp_attributes <- sp_list %>%
+  left_join(select(spp_raw, all_of(selected_cols_species)),
+            by = c("scientific_name" = "Species"))
 
-# Summarize growth rates, just get Linf and and K values
-growth_summary <- growth_fb %>%
+#------------------------------------------------------------------------------
+# Summarize growth data (K, Loo) by species
+
+growth_summary <- growth_raw %>%
   group_by(Species) %>%
   summarise(
     n_growth_studies = n(),
-    K_avg = mean(K, na.rm = TRUE),
+    K_avg   = mean(K, na.rm = TRUE),
     Loo_avg = mean(Loo, na.rm = TRUE),
     .groups = "drop"
   )
 
-sp_attritbutes <- # Join into attributes data frame
-  sp_attritbutes %>%
-  left_join(
-    growth_summary,
-    by = c("scientific_name" = "Species")
-  )
+sp_attributes <- sp_attributes %>%
+  left_join(growth_summary, by = c("scientific_name" = "Species"))
 
-# Pull selected columns to select from repro_db
+#------------------------------------------------------------------------------
+# Select and join reproduction fields
+
 selected_cols_repro <- c(
-  "Species", 
-  "ReproMode", "Spawning", 
+  "Species", "ReproMode", "Spawning", 
   "RepGuild1", "RepGuild2", "AddInfos"
 )
 
-# Left join the selected columns from repro_db to attributes dataframe
-sp_attritbutes <- 
-  sp_attritbutes %>%
-  left_join(
-    select(repro_fb, all_of(selected_cols_repro)),
-    by = c("scientific_name" = "Species")
-  )
+sp_attributes <- sp_attributes %>%
+  left_join(select(repro_raw, all_of(selected_cols_repro)),
+            by = c("scientific_name" = "Species"))
 
-# Pull selected columns to select from ecology_db
+#------------------------------------------------------------------------------
+# Select and join ecological traits (trophic level, remarks)
+
 selected_cols_ecology <- c(
-  "Species", 
-  "DietTroph", "DietSeTroph", 
+  "Species", "DietTroph", "DietSeTroph", 
   "DietRemark", "AddRems"
 )
 
-# Left join the selected columns from repro_db to attributes dataframe
-sp_attritbutes <- 
-  sp_attritbutes %>%
-  left_join(
-    select(ecology_fb, all_of(selected_cols_ecology)),
-    by = c("scientific_name" = "Species")
-  )
+sp_attributes <- sp_attributes %>%
+  left_join(select(ecology_raw, all_of(selected_cols_ecology)),
+            by = c("scientific_name" = "Species"))
 
-
-
+#------------------------------------------------------------------------------
+# Save output
+write.csv(sp_attributes, "fishbase_species_attributes.csv", row.names = FALSE)
