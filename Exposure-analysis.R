463947e506395dae5155e9fc82e86e584d9f2b5d
Holden Harris <holden.earl.harris@gmail.com>
2025-08-13 12:05:20 -0400

    Northern extent changed from 29 to 27.8. Cleaned up code.

--- Exposure-analysis.R
+++ Exposure-analysis.R
@@ -1,4 +1,11 @@
 ##------------------------------------------------------------------------------
+## Org:     Harris Analytics and Research LLC | Isla Mar
+## Project: Caribbean CVA
+## Contact: Holden Earl Harris | holden.earl.harris@gmail.com
+## Code:    Make Distribution Maps
+##------------------------------------------------------------------------------
+
+##------------------------------------------------------------------------------
 # Clear environment and load packages
 rm(list = ls()); gc()
 library(dplyr)
@@ -6,24 +13,22 @@ library(sf)
 library(ggplot2)
 library(dplyr)
 library(rnaturalearth)
-library(rnaturalearthdata)
-library(marmap)
+#library(rnaturalearthdata)
 library(raster)
 
 ## -----------------------------------------------------------------------------
 ## Configuration
 species_list <- read.csv("./data/master-lists/species-list.csv")
 
-
 ## -----------------------------------------------------------------------------
 ## Set geographic extent (bounding box)
 xlim_carib <- c(-92, -57)
-ylim_carib <- c(  6,  29)
+ylim_carib <- c(  6,  27.8)
 
 ## West (Yucatán Channel buffer): −92°
 ## East (just east of Barbados): −57°
 ## South (Panama/Colombia coast buffer): 6°N
-## North (covers all Bahamas): 29°N
+## North (covers all Bahamas): 27.8°N
 ## If you want a tighter box (less open Atlantic/Gulf water) use xlim = c(-90, -58) and ylim = c(7, 28).
 
 carib_bbox_sf <- sf::st_as_sfc( ## sf polygon for masking/overlays
@@ -37,7 +42,6 @@ world <- rnaturalearth::ne_countries(scale = "medium", returnclass = "sf")
 
 ## -----------------------------------------------------------------------------
 ## Functions to pull names from files
-
 name_from_shp <- function(path) { # Base path -> "Nice Name"
   base <- tools::file_path_sans_ext(basename(path))  # insert space between lower->Upper (CamelCase), e.g., "AtlanticHerring" -> "Atlantic Herring"
   base <- gsub("(?<=[a-z])(?=[A-Z])", " ", base, perl = TRUE)  # replace _, -, . with spaces
@@ -53,7 +57,6 @@ slugify <- function(name) { ## "Nice Name" -> "nice-name" (for filenames/ids)
   gsub("(^-|-$)", "", out)
 }
 
-
 ##------------------------------------------------------------------------------
 ## Loop through shape files
 
@@ -64,7 +67,7 @@ if (length(shp_files) == 0L) stop("No .shp files found in: ", shp_dir)
 
 ## Run loop
 for (i in 1:length(shp_files)){
-# for (i in 1:2){ ## Use for testing
+# for (i in 1:7){ ## Use for testing
   ## Set species
   shp <- shp_files[i]
   species_name <- name_from_shp(shp)
@@ -101,7 +104,6 @@ for (i in 1:length(shp_files)){
       axis.title = element_text(color = "black"),   ## axis title black (if not blank)
       axis.ticks   = element_line(color = "gray90") ## show axis ticks (theme_minimal hides them by default)
     )
-  
   
   ## Write out plot -----------------------------------------------------------
   out_dir <- "./outputs/disbribution-maps/"
