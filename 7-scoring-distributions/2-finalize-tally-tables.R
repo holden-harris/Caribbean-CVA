513d89090da96f8681786bb678f8d834fbb6f5cb
Holden E. Harris <holden.earl.harris@gmail.com>
2026-04-17 15:46:31 -0400

    minor name change

--- 7-scoring-distributions/2-finalize-tally-tables.R
+++ 7-scoring-distributions/2-finalize-tally-tables.R
@@ -156,7 +156,7 @@ write.csv(directional_effect_final,
 write.csv(exposure_tallies_long,
           file.path(dir_out, "exposure_tallies_long.csv"),           row.names = FALSE)
 write.csv(exposure_final,
-          file.path(dir_out, "exposure__tallies_by_stock.csv"),     row.names = FALSE)
+          file.path(dir_out, "exposure_tallies_by_stock.csv"),     row.names = FALSE)
 
 ################################################################################
 ## Row count summary
